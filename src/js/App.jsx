7c8c2337572a2500e909a4b0a6b927e0ec8eb0ee
Po-Yu Hsieh <pykenny@gmail.com>
2020-10-27 02:47:41 -0400

    Workable skeleton
    Details:
     - Fix HTML format issue
     - Change the default port for dev server
     - Avoid rendering React componet in `<body>` tag.
     - Solve package's peer dependency issue

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import MarkdownEditor from './markdown_editor/markdown_editor';
 
-ReactDOM.render(<MarkdownEditor/>, document.body);
+ReactDOM.render(<MarkdownEditor/>, document.getElementById('markdownEditorApp'));
