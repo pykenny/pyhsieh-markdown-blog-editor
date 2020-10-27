0c21c3b5367d9baa2eca4ba046e984c1dd2b9476
Po-Yu Hsieh <pykenny@gmail.com>
2020-10-27 02:28:52 -0400

    Some initial tries

---
+++ src/js/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MarkdownEditor from './markdown_editor/markdown_editor';
+
+ReactDOM.render(<MarkdownEditor/>, document.body);
---
+++ src/js/markdown_editor/markdown_editor.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Currently it's just a hollow block
+const MarkdownEditor = () => (
+  <h1>Hello!</h1>
+);
+
+export default MarkdownEditor;
