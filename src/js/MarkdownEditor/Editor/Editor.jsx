f162aff2ac6746b2de18059b963c2b5b4c0251cd
Po-Yu Hsieh <pykenny@gmail.com>
2020-10-28 15:46:51 -0400

    Adding TODO notes about parsing process

--- src/js/MarkdownEditor/Editor/Editor.jsx
+++ src/js/MarkdownEditor/Editor/Editor.jsx
@@ -13,6 +13,31 @@ function markdown2HTML(markdownStr) {
 
 function Editor(props) {
   const { onEditChange, logRecords, markdownStr } = props;
+
+  /* TODOs:
+   * There are several issues here for doing direct parsing here:
+   *  1. Deal with invalid image. The app should make request to ask about
+   *     file availability first before inserting HTML. Otherwise let the
+   *     preview area provide error information ("File <...> not available in
+   *     image content directory...")
+   *  2. We want to customize code block that supports language syntax
+   *     highlighting, which require external library support.
+   *  3. We want to prevent the editor from requesting local server too
+   *     often even if it's not really using user's network resource. Make
+   *     some delay between edit and parsing can help (for instance, update
+   *     only when the user stop modifying the edit area for 3 secoonds), but
+   *     we want some better control on reducing image requests to the server.
+   *
+   * Optional / Future Works:
+   *  1. Modify parser's syntax to support processing img parts like:
+   *     ![ALT_TEXT](URL_LINK|ALIAS)
+   *  2. Process alias (we can do this later)
+   *  3. Local caching for images. If repetitive URLs are found, then use the
+   *     cached image, instead of making server request. This also means we
+   *     need to manage the cache as internal state, and manipulate the HTML
+   *     content in preview area (for instance, cache the images in base64,
+   *     then set up the image block with 'src' attribute).
+   */
   const htmlStr = markdownStr ? markdown2HTML(markdownStr) : '';
 
   return (
