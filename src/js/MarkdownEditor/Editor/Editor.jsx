41c4414f8e949c7b1e5120c08cebbedc2a2c24d1
Po-Yu Hsieh <pykenny@gmail.com>
2020-10-29 18:06:17 -0400

    Support alias in image syntax!
    Details:
     - Define `Editor` component with class since we're setting
       inner attribute (the parser) when initialized
     - Use `markdown-it` instead of `marked` as parser because there's
       no way to change render's arguments (arity) without modifying
       source code. `markdown-it` allows us to append additional
       information element attribute by twisting output so that's
       feasible.
     - Initial attempt to add "alias" attribute to image elements.

--- src/js/MarkdownEditor/Editor/Editor.jsx
+++ src/js/MarkdownEditor/Editor/Editor.jsx
@@ -1,61 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import marked from 'marked';
-import DOMPurify from 'dompurify';
+import parseDocument, { createDocumentParser } from '../../helpers/parser';
 
 import LogArea from './LogArea';
 import EditArea from './EditArea';
 import PreviewArea from './PreviewArea';
 
-function markdown2HTML(markdownStr) {
-  return DOMPurify.sanitize(marked(markdownStr));
-}
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.parser = createDocumentParser();
+  }
 
-function Editor(props) {
-  const { onEditChange, logRecords, markdownStr } = props;
+  render() {
+    const { onEditChange, logRecords, markdownStr } = this.props;
+    const htmlStr = markdownStr ? parseDocument(markdownStr, this.parser) : '';
 
-  /* TODOs:
-   * There are several issues here for doing direct parsing here:
-   *  1. Deal with invalid image. The app should make request to ask about
-   *     file availability first before inserting HTML. Otherwise let the
-   *     preview area provide error information ("File <...> not available in
-   *     image content directory...")
-   *  2. We want to customize code block that supports language syntax
-   *     highlighting, which require external library support.
-   *  3. We want to prevent the editor from requesting local server too
-   *     often even if it's not really using user's network resource. Make
-   *     some delay between edit and parsing can help (for instance, update
-   *     only when the user stop modifying the edit area for 3 secoonds), but
-   *     we want some better control on reducing image requests to the server.
-   *
-   * Optional / Future Works:
-   *  1. Modify parser's syntax to support processing img parts like:
-   *     ![ALT_TEXT](URL_LINK|ALIAS)
-   *  2. Process alias (we can do this later)
-   *  3. Local caching for images. If repetitive URLs are found, then use the
-   *     cached image, instead of making server request. This also means we
-   *     need to manage the cache as internal state, and manipulate the HTML
-   *     content in preview area (for instance, cache the images in base64,
-   *     then set up the image block with 'src' attribute).
-   */
-  const htmlStr = markdownStr ? markdown2HTML(markdownStr) : '';
-
-  return (
-    <div className="editor-interface-container">
-      <div className="grid-row">
-        <div className="editor-interface-block  grid-column-half editor-input-container">
-          <div className="area-title">Editor</div>
-          <EditArea onEditChange={onEditChange} />
-          <div className="area-title">Log</div>
-          <LogArea logRecords={logRecords} />
-        </div>
-        <div className="editor-interface-block grid-column-half editor-output-container">
-          <div className="area-title">Parsed Result</div>
-          <PreviewArea htmlStr={htmlStr} />
+    return (
+      <div className="editor-interface-container">
+        <div className="grid-row">
+          <div className="editor-interface-block  grid-column-half editor-input-container">
+            <div className="area-title">Editor</div>
+            <EditArea onEditChange={onEditChange} />
+            <div className="area-title">Log</div>
+            <LogArea logRecords={logRecords} />
+          </div>
+          <div className="editor-interface-block grid-column-half editor-output-container">
+            <div className="area-title">Parsed Result</div>
+            <PreviewArea htmlStr={htmlStr} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Editor.propTypes = {
---
+++ src/js/helpers/parser.js
@@ -0,0 +1,283 @@
+/* eslint-disable no-param-reassign, no-plusplus, no-multi-assign, yoda */
+
+/* ^ Disable some eslint rules to reduce the efforts on modifying.
+ *   Still, we keep the variable declaration checking enabled to better track
+ *   the variables (constance and inner-scope variables)
+ */
+
+import MarkdownIt from 'markdown-it';
+import { normalizeReference, isSpace } from 'markdown-it/lib/common/utils';
+import { isArray, includes } from 'lodash';
+
+function isValidAliasChar(code, isStartChar) {
+  /* Currently alias only allows string that fits the regex below:
+   * /[A-Za-z][A-Za-z0-9\-]+/
+   * 1. The first character should be English alphabet
+   * 2. The rest of them should be either English alphabet, decimal digit,
+   *    or hyphen ('-').
+   */
+  return isStartChar
+    ? (0x41 <= code && code <= 0x5A) || ((0x61 <= code) && (code <= 0x7A))
+    : ((0x30 <= code) && (code <= 0x39))
+      || ((0x41 <= code) && (code <= 0x5A))
+      || ((0x61 <= code) && (code <= 0x7A))
+      || (code === 0x2D);
+}
+
+function isValidAlias(str) {
+  let pos = 0;
+  const max = str.length;
+  while (pos < max) {
+    if (!isValidAliasChar(str.charCodeAt(pos), pos === max)) {
+      return false;
+    }
+    pos++;
+  }
+  // Alias can't be empty string
+  return !(pos === max);
+}
+
+function parseAlias(str, pos, max, exitCharCodeList) {
+  let code;
+  const start = pos;
+  const result = {
+    ok: false,
+    pos: 0,
+    str: '',
+  };
+
+  if (!isArray(exitCharCodeList)) {
+    exitCharCodeList = [];
+  }
+
+  while (pos < max) {
+    code = str.charCodeAt(pos);
+    // When hitting space or of one of the exit char code, wrap up and return
+    if (isSpace(code) || includes(exitCharCodeList, code)) {
+      result.pos = pos;
+      result.str = str.slice(start, pos);
+      result.ok = true;
+      return result;
+    }
+    // If it's valid character, keep consuming the string
+    if (isValidAliasChar(code, start === pos)) {
+      pos++;
+    } else {
+      return result;
+    }
+  }
+  // When used up the string before hitting exit boundary, return failure
+  return result;
+}
+
+function imageWithAlias(state, silent) {
+  const labelEnd = state.md.helpers.parseLinkLabel(state, state.pos + 1, false);
+  const labelStart = state.pos + 2;
+  const oldPos = state.pos;
+  const max = state.posMax;
+
+  let attrs;
+  let code;
+  let content;
+  let label;
+
+  let ref;
+  let res;
+  let title;
+  let token;
+  let tokens;
+  let start;
+  let alias;
+  let href = '';
+
+  if (state.src.charCodeAt(state.pos) !== 0x21/* ! */) { return false; }
+  if (state.src.charCodeAt(state.pos + 1) !== 0x5B/* [ */) { return false; }
+
+  // parser failed to find ']', so it's not a valid link
+  if (labelEnd < 0) { return false; }
+
+  let pos = labelEnd + 1;
+  if (pos < max && state.src.charCodeAt(pos) === 0x28/* ( */) {
+    //
+    // Inline link
+    //
+
+    // [link](  <href>  "title"  )
+    //        ^^ skipping these spaces
+    pos++;
+    for (; pos < max; pos++) {
+      code = state.src.charCodeAt(pos);
+      if (!isSpace(code) && code !== 0x0A) { break; }
+    }
+    if (pos >= max) { return false; }
+
+    // [link](  <href>  "title"  )
+    //          ^^^^^^ parsing link destination
+    start = pos;
+    res = state.md.helpers.parseLinkDestination(state.src, pos, state.posMax);
+    if (res.ok) {
+      href = state.md.normalizeLink(res.str);
+      if (state.md.validateLink(href)) {
+        pos = res.pos;
+      } else {
+        href = '';
+      }
+    }
+
+    // [link](  <href>  "title"  )
+    //                ^^ skipping these spaces
+    start = pos;
+    for (; pos < max; pos++) {
+      code = state.src.charCodeAt(pos);
+      if (!isSpace(code) && code !== 0x0A) { break; }
+    }
+
+    // [link](  <href>  "title"  )
+    //                  ^^^^^^^ parsing link title
+    res = state.md.helpers.parseLinkTitle(state.src, pos, state.posMax);
+    if (pos < max && start !== pos && res.ok) {
+      title = res.str;
+      pos = res.pos;
+
+      // [link](  <href>  "title"  )
+      //                         ^^ skipping these spaces
+      for (; pos < max; pos++) {
+        code = state.src.charCodeAt(pos);
+        if (!isSpace(code) && code !== 0x0A) { break; }
+      }
+    } else {
+      title = '';
+    }
+
+    /* Note: A pitfall here is that we're unable to process inline link written
+     *       in format where title is skipped and pipe symbol comes immediately
+     *       after href, such as ![alt](/some/link|some-alias). It's because
+     *       helpers.parseLinkDestination() will consume pipe symbol and try
+     *       escape it to make a valid URI. We can try rewrite the method but
+     *       it may conflict with other rulers that depends on this function.
+     *
+     *       Right now to skip title, always place at least one blank between
+     *       the link and pipe symbol, for instance,
+     *       ![alt](/some/link |some-alias) or ![alt](/some/link | some-alias)
+     */
+
+    // [link](  <href>  "title"  |  alias  )
+    //                           ^ detect boundary of alias
+    if (pos < max && state.src.charCodeAt(pos) === 0x7C/* | */) {
+      pos++;
+      // [link](  <href>  "title"  |  alias  )
+      //                            ^^ skip spaces before the alias
+      for (; pos < max; pos++) {
+        code = state.src.charCodeAt(pos);
+        if (!isSpace(code) && code !== 0x0A) { break; }
+      }
+      // [link](  <href>  "title"  |  alias  )
+      //                              ^^^^^ parse alias
+      start = pos;
+      res = parseAlias(state.src, pos, state.posMax, [0x29]);
+      if (pos < max && res.pos !== start && res.ok) {
+        alias = res.str;
+        pos = res.pos;
+      } else {
+        /* Taking a strict stance here: Either not initiating alias declaration
+         * within inline link, or providing one valid alais.
+         * By doing so, syntax issues will be handled before alias resolution.
+         */
+        state.pos = oldPos;
+        return false;
+      }
+      // [link](  <href>  "title"  |  alias  )
+      //                                   ^^ skip spaces in between
+      for (; pos < max; pos++) {
+        code = state.src.charCodeAt(pos);
+        if (!isSpace(code) && code !== 0x0A) { break; }
+      }
+    }
+
+    if (pos >= max || state.src.charCodeAt(pos) !== 0x29/* ) */) {
+      state.pos = oldPos;
+      return false;
+    }
+    pos++;
+  } else {
+    //
+    // Link reference
+    //
+    if (typeof state.env.references === 'undefined') { return false; }
+
+    if (pos < max && state.src.charCodeAt(pos) === 0x5B/* [ */) {
+      start = pos + 1;
+      pos = state.md.helpers.parseLinkLabel(state, pos);
+      if (pos >= 0) {
+        label = state.src.slice(start, pos++);
+      } else {
+        pos = labelEnd + 1;
+      }
+    } else {
+      pos = labelEnd + 1;
+    }
+
+    // covers label === '' and label === undefined
+    // (collapsed reference link and shortcut reference link respectively)
+    if (!label) { label = state.src.slice(labelStart, labelEnd); }
+
+    // Note: We're restricting the reference lebel to be in line with alias
+    // format requirements, so that we can use the label for alias directly
+    // when written in this form.
+    label = label.trim();
+    ref = isValidAlias(label) && state.env.references[normalizeReference(label)];
+    if (!ref) {
+      state.pos = oldPos;
+      return false;
+    }
+    alias = label;
+    href = ref.href;
+    title = ref.title;
+  }
+
+  //
+  // We found the end of the link, and know for a fact it's a valid link;
+  // so all that's left to do is to call tokenizer.
+  //
+  if (!silent) {
+    content = state.src.slice(labelStart, labelEnd);
+
+    state.md.inline.parse(
+      content,
+      state.md,
+      state.env,
+      tokens = [],
+    );
+
+    token = state.push('image', 'img', 0);
+    token.attrs = attrs = [['src', href], ['alt', ''], ['alias', alias]];
+    token.children = tokens;
+    token.content = content;
+
+    if (title) {
+      attrs.push(['title', title]);
+    }
+  }
+
+  state.pos = pos;
+  state.posMax = max;
+
+  return true;
+}
+
+function createDocumentParser() {
+  // Default parser settings with img parser overwritten
+  const parser = MarkdownIt();
+  parser.inline.ruler.at('image', imageWithAlias);
+  return parser;
+}
+
+function parseDocument(str, psr) {
+  const parser = psr || createDocumentParser();
+  return parser.render(str);
+}
+
+export {
+  parseDocument as default,
+  createDocumentParser,
+};
